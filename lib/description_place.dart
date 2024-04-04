ec93c6f43c7419fe4555fc2defa55106c50cac05
Danniel Navas <dannielnavas@gmail.com>
2024-04-04 07:41:38 -0500

    ✨ Añadir importación de ButtonPurple y ajustar diseño en DescriptionPlace

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tripix/button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget {
   final String namePlace;
@@ -71,11 +72,8 @@ class DescriptionPlace extends StatelessWidget {
     );
 
     return Column(
-      // crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        titleStars,
-        description,
-      ],
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[titleStars, description, ButtonPurple('Navigate')],
     );
   }
 }
